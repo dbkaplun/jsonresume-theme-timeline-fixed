377d098fdf04ff662a4ae005c230362da684cea5
Dan Kaplun <dan@beardtree.com>
2014-09-17 00:40:23 -0500

    Deletes irrelevant files from project

--- example/js/chart.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-$('.chart').easyPieChart({
-    easing: 'easeOutBounce',
-    onStep: function(from, to, percent) {
-        $(this.el).find('.percent').text(Math.round(percent));
-    }
-});
-var chart = window.chart = $('.chart').data('easyPieChart');
-});
